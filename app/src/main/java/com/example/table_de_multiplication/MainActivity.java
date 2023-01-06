75dec07299c999591f74a60b4fa704b6bf4c2c9c
abdelilah-Falih <abdelilah.falih.development@gmail.com>
2023-01-06 22:16:24 +0100

    resolve bugs from main

--- app/src/main/java/com/example/table_de_multiplication/MainActivity.java
+++ app/src/main/java/com/example/table_de_multiplication/MainActivity.java
@@ -8,16 +8,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btnEffacer,btnAfficher,btnQuitter,btnColor1,btnColor2,btnColor3;
     EditText nbrSaisi;
     ConstraintLayout layoutT;
+    TextView textAfficher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Table De Multiplication ");
         btnEffacer = findViewById(R.id.btnEffacer);
         btnAfficher = findViewById(R.id.btnAfficher);
         btnQuitter = findViewById(R.id.btnQuitter);
@@ -44,11 +47,13 @@ public class MainActivity extends AppCompatActivity {
                 layoutT.setBackgroundColor(0xFFBB86FC);
             }
         });
+        textAfficher = findViewById(R.id.textAfficher);
 
         btnEffacer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 nbrSaisi.setText("");
+                textAfficher.setText("? * 0 = ? \n ? * 1 = ? \n ? * 2 = ? \n ? * 3 = ? \n ? * 4 = ? \n ? * 5 = ? \n ? * 6 = ? \n ? * 7 = ? \n ? * 8 = ? \n ? * 9 = ? \n ? * 10 = ?");
             }
         });
         btnQuitter.setOnClickListener(new View.OnClickListener() {
