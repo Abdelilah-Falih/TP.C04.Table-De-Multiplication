349c93a39be550db7592de663fe0d0ef4c0e0934
abdelilah-Falih <abdelilah.falih.development@gmail.com>
2023-01-06 12:07:55 +0100

    button reinitialiser et quitter son activer

--- app/src/main/java/com/example/table_de_multiplication/MainActivity.java
+++ app/src/main/java/com/example/table_de_multiplication/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.table_de_multiplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnEffacer,btnAfficher,btnQuitter;
+    EditText nbrSaisi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnEffacer = findViewById(R.id.btnEffacer);
+        btnAfficher = findViewById(R.id.btnAfficher);
+        btnQuitter = findViewById(R.id.btnQuitter);
+        nbrSaisi = findViewById(R.id.nbrSaisi);
+        btnEffacer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nbrSaisi.setText("");
+            }
+        });
+        btnQuitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
