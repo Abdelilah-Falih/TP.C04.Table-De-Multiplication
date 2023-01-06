2096fcbabb9a3915fdc0bc6afd4312cb8e4ac63a
abdelilah-Falih <abdelilah.falih.development@gmail.com>
2023-01-06 22:16:24 +0100

    bugs(title |btnReinitialiser |txtView id) fixed

--- app/src/main/java/com/example/table_de_multiplication/MainActivity.java
+++ app/src/main/java/com/example/table_de_multiplication/MainActivity.java
@@ -6,22 +6,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btnEffacer,btnAfficher,btnQuitter;
     EditText nbrSaisi;
+    TextView textAfficher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Table De Multiplication ");
         btnEffacer = findViewById(R.id.btnEffacer);
         btnAfficher = findViewById(R.id.btnAfficher);
         btnQuitter = findViewById(R.id.btnQuitter);
         nbrSaisi = findViewById(R.id.nbrSaisi);
+        textAfficher = findViewById(R.id.textAfficher);
+
         btnEffacer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 nbrSaisi.setText("");
+                textAfficher.setText("? * 0 = ? \n ? * 1 = ? \n ? * 2 = ? \n ? * 3 = ? \n ? * 4 = ? \n ? * 5 = ? \n ? * 6 = ? \n ? * 7 = ? \n ? * 8 = ? \n ? * 9 = ? \n ? * 10 = ?");
             }
         });
         btnQuitter.setOnClickListener(new View.OnClickListener() {
