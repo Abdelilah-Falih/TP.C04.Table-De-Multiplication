5866bf939e1fe3935ce942baf27c918412b0be41
abdelilah-Falih <abdelilah.falih.development@gmail.com>
2023-01-06 22:01:50 +0100

    les button de coloriage son actives

--- app/src/main/java/com/example/table_de_multiplication/MainActivity.java
+++ app/src/main/java/com/example/table_de_multiplication/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.table_de_multiplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnEffacer,btnAfficher,btnQuitter;
+    Button btnEffacer,btnAfficher,btnQuitter,btnColor1,btnColor2,btnColor3;
     EditText nbrSaisi;
+    ConstraintLayout layoutT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +22,29 @@ public class MainActivity extends AppCompatActivity {
         btnAfficher = findViewById(R.id.btnAfficher);
         btnQuitter = findViewById(R.id.btnQuitter);
         nbrSaisi = findViewById(R.id.nbrSaisi);
+        btnColor1 = findViewById(R.id.btnColor1);
+        btnColor2 = findViewById(R.id.btnColor2);
+        btnColor3 = findViewById(R.id.btnColor3);
+        layoutT  = findViewById(R.id.layoutT);
+        btnColor1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutT.setBackgroundColor(Color.WHITE);
+            }
+        });
+        btnColor2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutT.setBackgroundColor(0x9FF3A5D6);
+            }
+        });
+        btnColor3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutT.setBackgroundColor(0xFFBB86FC);
+            }
+        });
+
         btnEffacer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
